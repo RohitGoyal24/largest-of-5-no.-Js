ba95e67cc7fb87a704bd48dcf9e955ab55826bba
RohitGoyal24 <39431001+RohitGoyal24@users.noreply.github.com>
2018-12-16 14:08:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var num1=-8;
+var num2=12.5;
+var num3=-6.769;
+var num4=0;
+var num5=-1;
+if(num1>num2){
+    var g1=num1;
+}
+else {
+    var g1=num2;
+}
+if(g1>num3)
+{
+    var g2=g1;
+}
+else{
+     g2=num3;
+}
+if(g2>num4)
+{
+    var g3=g2;
+}
+else{
+    g3=num4;
+}
+if(g3>num5)
+{
+    var g4=g3;
+}
+else{
+    g4=num5;
+}
+console.log(g4);
